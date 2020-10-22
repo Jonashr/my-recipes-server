b647f036e30cd14333347d661c16baf64e379d87
Jonas Horvei <jonashorvei1@gmail.com>
2020-10-22 18:13:04 +0200

    Added a procfile

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const app = require('./app')
 const http = require('http')
 const server = http.createServer(app)
 const PORT = process.env.PORT || 3000
 
+console.log('process.env', process.env.PORT)
 
 server.listen(PORT, () => {
   console.log(`Server now running on port ${PORT}`)
